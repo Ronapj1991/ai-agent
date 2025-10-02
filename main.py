36e18ea8e31168b8501d6df5e2857e6690c65c6f
Ronapj1991 <Ronapj1991@gmail.com>
2025-10-02 02:55:05 +0000

    add list of messages to keep track of conversation

---
+++ main.py
@@ -0,0 +1,38 @@
+import os
+import sys
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
+def main():
+    load_dotenv() #pulls the API key from .env
+    
+    args = sys.argv[1:] #returrns a list of arguments. Each item will be what is after the script name when ran
+    
+    if not args: 
+        print("Usage: uv run main.py <prompt>")
+        sys.exit(1)
+    
+    api_key = os.environ.get("GEMINI_API_KEY") #if not for load_dotenv this will return None
+    client = genai.Client(api_key=api_key) # instantiate Google Generative AI Python SDK package with my own API key
+    
+    user_prompt = " ".join(args) # turns everything after the script name into one string
+    
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]) #found in https://googleapis.github.io/python-genai/genai.html#genai.types.ApiAuthApiKeyConfigDict under genai.live module
+    ]
+    
+    generate_content(client, messages)
+
+def generate_content(client, messages):
+    response = client.models.generate_content(
+        model='gemini-2.0-flash-001', 
+        contents=messages
+    )
+
+    print(response.text)
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+if __name__ == "__main__":
+    main()
