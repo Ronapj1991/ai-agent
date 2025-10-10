37534fb8e72a9de03a64760e356317bb35843154
Ronapj1991 <Ronapj1991@gmail.com>
2025-10-10 03:33:06 +0000

    add helper function for listing contents of dir

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -3,9 +3,20 @@ import os
 def get_files_info(working_directory, directory="."):
     full_path = os.path.abspath(os.path.join(working_directory, directory))
     wd = os.path.abspath(working_directory)
+    
     if not full_path.startswith(wd):
         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
     if not os.path.isdir(full_path):
         return f'Error: "{directory}" is not a directory'
+    
+    
+    
 
-get_files_info("mydir", "new_folder")
+def contents_str_builder(full_path):
+    dir_contents = ""
+    for file in os.listdir(full_path):
+        file_path = os.path.join(full_path, file)
+        file_size = os.path.getsize(file_path)
+        is_dir = os.path.isdir(file_path)
+        dir_contents += f"- {file}: file_size={file_size} bytes, is_dir={is_dir}\n"
+    return dir_contents
