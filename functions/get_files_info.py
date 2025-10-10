6327d29a76bba3b618fd2fdbf7a57fd67295750d
Ronapj1991 <Ronapj1991@gmail.com>
2025-10-10 03:07:14 +0000

    add check if directory is present

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -5,5 +5,7 @@ def get_files_info(working_directory, directory="."):
     wd = os.path.abspath(working_directory)
     if not full_path.startswith(wd):
         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+    if not os.path.isdir(full_path):
+        return f'Error: "{directory}" is not a directory'
 
 get_files_info("mydir", "new_folder")
