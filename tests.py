26f799bfd53e61c6ee8b96ece5d6439439b91478
Ronapj1991 <Ronapj1991@gmail.com>
2025-10-12 22:03:24 +0000

    fix import bug

--- tests.py
+++ tests.py
@@ -1,21 +1,13 @@
-from functions.get_files_info import get_files_info
+from functions.get_files_content import get_file_content
 
-# Test 1
-result = get_files_info("calculator", ".")
-print("Result for current directory:")
+result = get_file_content("calculator", "main.py")
 print(result)
 
-# Test 2
-result = get_files_info("calculator", "pkg")
-print("Result for 'pkg' directory:")
+result = get_file_content("calculator", "pkg/calculator.py")
 print(result)
 
-# Test 3
-result = get_files_info("calculator", "/bin")
-print("Result for '/bin' directory:")
+result = get_file_content("calculator", "/bin/cat")
 print(result)
 
-# Test 4
-result = get_files_info("calculator", "../")
-print("Result for '../' directory:")
+result = get_file_content("calculator", "pkg/does_not_exist.py")
 print(result)
