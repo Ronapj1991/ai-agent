9099a83413b18751051ec552f3f671291a40ad2c
Ronapj1991 <Ronapj1991@gmail.com>
2025-10-12 17:56:58 +0000

    add file reader with Max length

--- functions/get_files_content.py
+++ functions/get_files_content.py
@@ -1,4 +1,5 @@
 import os
+from config import MAX_CHARS
 
 def get_file_content(working_directory, file_path):
     wd_path = os.path.abspath(working_directory)
@@ -8,3 +9,13 @@ def get_file_content(working_directory, file_path):
     if not os.path.isfile(full_path):
         return f'Error: File not found or is not a regular file: "{file_path}"'
     
+    try:
+        with open(full_path, "r") as f:
+            file_content_string = f.read(MAX_CHARS + 1)
+    except Exception as e:
+        return f"Error: {e}"
+    
+    if len(file_content_string) > MAX_CHARS:
+        return f'{file_content_string[:MAX_CHARS]}[...File "{file_path}" truncated at {MAX_CHARS} characters]'
+    
+    return file_content_string
