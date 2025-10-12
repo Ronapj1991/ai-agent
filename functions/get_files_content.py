49f8d91be92683589d3ec3784de06536f2b66c2a
Ronapj1991 <Ronapj1991@gmail.com>
2025-10-12 17:18:09 +0000

    add check if file is valid

--- functions/get_files_content.py
+++ functions/get_files_content.py
@@ -5,3 +5,6 @@ def get_file_content(working_directory, file_path):
     full_path = os.path.abspath(os.path.join(wd_path, file_path))
     if not full_path.startswith(wd_path):
         return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+    if not os.path.isfile(full_path):
+        return f'Error: File not found or is not a regular file: "{file_path}"'
+    
