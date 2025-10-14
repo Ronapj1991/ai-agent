e55fd34461d12d76517ec3b8c9fccabbe0412e4b
Ronapj1991 <Ronapj1991@gmail.com>
2025-10-14 14:37:48 +0000

    add feature to write to a file even if it exists

--- functions/write_file.py
+++ functions/write_file.py
@@ -2,9 +2,16 @@ import os
 
 def write_file(working_directory, file_path, content):
     wd_path = os.path.abspath(working_directory) 
-    fp_full = os.path.abspath(file_path)
+    fp_full = os.path.abspath(os.path.join(wd_path, file_path))
     
-    if not fp_full.startswith(wd_path):
+    if os.path.commonpath([wd_path, fp_full]) != wd_path:
         return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
     
-    pass
+    os.makedirs(os.path.dirname(fp_full), exist_ok=True)
+    try:
+        with open(fp_full, "w") as f:
+            f.write(content)
+        return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+    except Exception as e:
+        return f"Error: {e}"
+    
