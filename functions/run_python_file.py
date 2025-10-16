4850c908ddec7f0c18bb1b6c8dfd006ccfa84aaf
Ronapj1991 <Ronapj1991@gmail.com>
2025-10-16 03:13:41 +0000

    add check if the file ends with py

--- functions/run_python_file.py
+++ functions/run_python_file.py
@@ -8,5 +8,7 @@ def run_python_file(working_directory, file_path, args=[]):
         return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
     if not os.path.exists(full_path):
         return f'Error: File "{file_path}" not found.'
+    if not full_path.endswith(".py"):
+        return f'Error: "{file_path}" is not a Python file.'
     
     pass
