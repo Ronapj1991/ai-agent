853376d632fb58e2e9668c62df95f65a15d12c9c
Ronapj1991 <Ronapj1991@gmail.com>
2025-10-20 03:18:50 +0000

    add tests

--- tests.py
+++ tests.py
@@ -1,19 +1,12 @@
+import os
+from functions.get_file_content import get_file_content
+from functions.get_files_info import get_files_info
+from functions.write_file import write_file
 from functions.run_python_file import run_python_file
 
-result = run_python_file("calculator", "main.py")
-print(result)
+wd = os.getcwd()
 
-result = run_python_file("calculator", "main.py", ["3 + 5"])
-print(result)
-
-result = run_python_file("calculator", "tests.py")
-print(result)
-
-result = run_python_file("calculator", "../main.py")
-print(result)
-
-result = run_python_file("calculator", "nonexistent.py")
-print(result)
-
-result = run_python_file("calculator", "lorem.txt")
-print(result)
+print(get_file_content(wd, "main.py"))
+print(write_file(wd, "main.txt", "hello"))
+print(run_python_file(wd, "main.py"))
+print(get_files_info(wd, "pkg"))
