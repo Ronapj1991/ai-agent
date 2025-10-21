94fb5b9b078baaa1ba17137c3cb7b0c44b1ac58e
Ronapj1991 <Ronapj1991@gmail.com>
2025-10-21 02:21:17 +0000

    add call function

---
+++ functions/call_functions.py
@@ -0,0 +1,22 @@
+from functions.get_file_content import get_file_content
+from functions.get_files_info import get_files_info
+from functions.run_python_file import run_python_file
+from functions.write_file import write_file
+
+def call_function(function_call_part, verbose=False):
+    if verbose:
+        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+    else:
+        print(f" - Calling function: {function_call_part.name}")
+    
+    function_dict = {
+        "get_file_content": get_file_content,
+        "get_files_info": get_files_info,
+        "run_python_file": run_python_file,
+        "write_file": write_file
+    }
+    
+    func = function_dict.get(function_call_part.name)
+    new_kwargs = {**function_call_part.args, "working_directory":"./calculator"}
+    result = func(**new_kwargs)
+    
